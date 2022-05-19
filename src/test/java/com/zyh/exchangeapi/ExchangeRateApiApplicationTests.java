52e757075e6002304ddbc3714d7d278f83f6d784
yinghan <yinghan@Yangs-MacBook-Pro.local>
2022-05-19 10:26:34 +0200

    inite spring project

---
+++ src/test/java/com/zyh/exchangeapi/ExchangeRateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyh.exchangeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
